508e649e9b9433273443338d24014975f99b9594
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-12-23 15:22:31 +0900

    UPDATE : Swipe & Click Logic

---
+++ MiniGame/Assets/Project_1/Scripts/MainPlayer_1.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer_1 : MonoBehaviour
+{
+    private Vector2 startPos;
+    private Vector2 endPos;
+
+    private Socks target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스 클릭 (터치) 할 경우
+        if (Input.GetMouseButtonDown(0))
+        {
+            // 마우스 클릭(터치) 한 포지션을, 2D 월드 포지션으로 변환
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // 클릭한 마우스 위치에서 레이캐스트에 걸리는 물체를 hit으로 가져온다
+            RaycastHit2D hit = Physics2D.Raycast(touchPosition, transform.forward, 15f);
+            if (hit)
+            {
+                Socks target = hit.transform.GetComponent<Socks>();
+                if (!target) return;
+
+                // 유채색 양말일 경우
+                if (target.isColorSocks)
+                {
+                    // 터치해서 움직이도록
+                    target.ClickSocks();
+                }
+                // 무채색 양말일 경우
+                else
+                {
+                    // 스와이프해서 움직이기 위해서 현재 마우스 포지션 저장
+                    this.startPos = Input.mousePosition;
+                    this.target = target;
+                }
+                
+                
+
+            }
+        }
+        // 마우스 클릭 (터치) 땔 경우
+        else if (Input.GetMouseButtonUp(0))
+        {
+            // 타겟이 없으면 무채색 양말이 아닌 경우 이므로
+            if (!target) return;
+
+            // 유채색 양말일 경우
+            this.endPos = Input.mousePosition;
+            Vector2 dir = (this.endPos - this.startPos).normalized;
+            Debug.Log("swipeDir = " + dir);
+            target.SwipeSocks(dir);
+
+            // 움직이도록 하고 변수 초기ㅎ
+            this.target = null;
+            this.startPos = Vector2.zero;
+            this.endPos = Vector2.zero;
+        }
+
+        
+    }
+}
--- MiniGame/Assets/Project_1/Scripts/Socks.cs
+++ MiniGame/Assets/Project_1/Scripts/Socks.cs
@@ -6,10 +6,13 @@ public class Socks : MonoBehaviour
 {
     // 색상이 있는 양말인지 아닌지
     public bool isColorSocks;
+    private float speed = 0.5f;
+
+    private Rigidbody2D rigid;
 
     void Start()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -18,7 +21,7 @@ public class Socks : MonoBehaviour
     }
 
     // 메인플레이어가 양말을 터치했을때 (색상이 있는 양말일 경우)
-    void ClickSocks()
+    public void ClickSocks()
     {
         if (!isColorSocks) return;
         Debug.Log("색상이 있는 양말입니다.");
@@ -26,9 +29,12 @@ public class Socks : MonoBehaviour
     }
 
     // 메인플레이어가 양말을 스와이프 했을 때 (무채색 양말일 경우)
-    void SwipeSocks()
+    public void SwipeSocks(Vector2 dir)
     {
         if (isColorSocks) return;
         Debug.Log("무채색 양말입니다");
+
+        // 전달 받은 힘만큼 방향으로 이동
+        rigid.AddForce(dir * speed, ForceMode2D.Impulse);
     }
 }
